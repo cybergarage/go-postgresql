b6fdbbd393fd8c16fc6e9d9f2666d66bce6cbb3c
Satoshi Konno <skonno@cybergarage.org>
2023-06-18 01:40:51 +0900

    Add message.NewBindComplete()

---
+++ postgresql/protocol/message/bind_complete.go
@@ -0,0 +1,20 @@
+// PostgreSQL: Documentation: 16: 55.2. Message Flow
+// https://www.postgresql.org/docs/16/protocol-flow.html
+// PostgreSQL: Documentation: 16: 55.7. Message Formats
+// https://www.postgresql.org/docs/16/protocol-message-formats.html
+// PostgreSQL: Documentation: 16: 55.8. Error and Notice Message Fields
+// https://www.postgresql.org/docs/16/protocol-error-fields.html
+
+package message
+
+// BindComplete represents an bind complete message.
+type BindComplete struct {
+	*ResponseMessage
+}
+
+// NewBindComplete returns a new bind complete message instance.
+func NewBindComplete() *BindComplete {
+	return &BindComplete{
+		ResponseMessage: NewResponseMessageWith(BindCompleteMessage),
+	}
+}
