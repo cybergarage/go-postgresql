66dc4cd6aed8c5c1d227a2b54ff1245e4acfd08a
Satoshi Konno <skonno@cybergarage.org>
2023-07-12 08:54:42 +0900

    Update BaseQueryExecutor

--- postgresql/executor_query.go
+++ postgresql/executor_query.go
@@ -30,45 +30,45 @@ func NewBaseQueryExecutor() *BaseQueryExecutor {
 
 // CreateDatabase handles a CREATE DATABASE query.
 func (executor *BaseQueryExecutor) CreateDatabase(*Conn, *query.CreateDatabase) ([]message.Response, error) {
-	return nil, ErrNotImplemented
+	return nil, newErrNotImplemented("CREATE DATABASE")
 }
 
 // CreateTable handles a CREATE TABLE query.
 func (executor *BaseQueryExecutor) CreateTable(*Conn, *query.CreateTable) ([]message.Response, error) {
-	return nil, ErrNotImplemented
+	return nil, newErrNotImplemented("CREATE TABLE")
 }
 
 // CreateIndex handles a CREATE INDEX query.
 func (executor *BaseQueryExecutor) CreateIndex(*Conn, *query.CreateIndex) ([]message.Response, error) {
-	return nil, ErrNotImplemented
+	return nil, newErrNotImplemented("CREATE INDEX")
 }
 
 // DropDatabase handles a DROP DATABASE query.
 func (executor *BaseQueryExecutor) DropDatabase(*Conn, *query.DropDatabase) ([]message.Response, error) {
-	return nil, ErrNotImplemented
+	return nil, newErrNotImplemented("DROP DATABASE")
 }
 
 // DropIndex handles a DROP INDEX query.
 func (executor *BaseQueryExecutor) DropTable(*Conn, *query.DropTable) ([]message.Response, error) {
-	return nil, ErrNotImplemented
+	return nil, newErrNotImplemented("DROP TABLE")
 }
 
 // Insert handles a INSERT query.
 func (executor *BaseQueryExecutor) Insert(*Conn, *query.Insert) ([]message.Response, error) {
-	return nil, ErrNotImplemented
+	return nil, newErrNotImplemented("INSERT")
 }
 
 // Select handles a SELECT query.
 func (executor *BaseQueryExecutor) Select(*Conn, *query.Select) ([]message.Response, error) {
-	return nil, ErrNotImplemented
+	return nil, newErrNotImplemented("SELECT")
 }
 
 // Update handles a UPDATE query.
 func (executor *BaseQueryExecutor) Update(*Conn, *query.Update) ([]message.Response, error) {
-	return nil, ErrNotImplemented
+	return nil, newErrNotImplemented("UPDATE")
 }
 
 // Delete handles a DELETE query.
 func (executor *BaseQueryExecutor) Delete(*Conn, *query.Delete) ([]message.Response, error) {
-	return nil, ErrNotImplemented
+	return nil, newErrNotImplemented("DELETE")
 }
