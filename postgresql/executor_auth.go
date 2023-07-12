54ec5ee8a4dceaf1427cba17b305b7f1ff9c2e03
Satoshi Konno <skonno@cybergarage.org>
2023-07-12 12:35:51 +0900

    Add BaseAuthenticator

---
+++ postgresql/executor_auth.go
@@ -0,0 +1,33 @@
+// Copyright (C) 2019 Satoshi Konno. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package postgresql
+
+import (
+	"github.com/cybergarage/go-postgresql/postgresql/protocol/message"
+)
+
+// BaseAuthenticator represents a base frontend message authenticator.
+type BaseAuthenticator struct {
+}
+
+// NewBaseAuthenticator returns a base frontend message authenticator.
+func NewBaseAuthenticator() *BaseAuthenticator {
+	return &BaseAuthenticator{}
+}
+
+// Authenticate authenticates the connection with the startup message.
+func (executor *BaseAuthenticator) Authenticate(*Conn, *message.Startup) (message.Response, error) {
+	return message.NewAuthenticationOk()
+}
