3fb2eba7ec0d1b3fa0bf2812e4a6312d37afd0c8
Satoshi Konno <skonno@cybergarage.org>
2023-07-13 12:23:01 +0900

    Update message.NewBindWith()

--- postgresql/protocol/message/bind.go
+++ postgresql/protocol/message/bind.go
@@ -51,16 +51,33 @@ func NewBindWith(reader *Reader, parse *Parse) (*Bind, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	// The name of the destination portal (an empty string selects the unnamed portal).
 	portal, err := reader.ReadString()
 	if err != nil {
 		return nil, err
 	}
 
+	// The name of the source prepared statement (an empty string selects the unnamed prepared statement).
 	name, err := reader.ReadString()
 	if err != nil {
 		return nil, err
 	}
 
+	// The number of parameter format codes that follow (denoted C below).
+	// This can be zero to indicate that there are no parameters or that the parameters all use the default format (text); or one, in which case the specified format code is applied to all parameters; or it can equal the actual number of parameters.
+	_, err = reader.ReadInt16()
+	if err != nil {
+		return nil, err
+	}
+
+	// The parameter format codes. Each must presently be zero (text) or one (binary).
+	t, err := reader.ReadInt16()
+	if err != nil {
+		return nil, err
+	}
+
+	// The number of parameter values that follow (possibly zero). This must match the number of parameters needed by the query.
 	num, err := reader.ReadInt16()
 	if err != nil {
 		return nil, err
@@ -68,25 +85,31 @@ func NewBindWith(reader *Reader, parse *Parse) (*Bind, error) {
 
 	params := make([]*BindParam, num)
 	for n := int16(0); n < num; n++ {
-		t, err := reader.ReadInt16()
-		if err != nil {
-			return nil, err
-		}
-		num, err := reader.ReadInt16()
-		if err != nil {
-			return nil, err
-		}
+		// The length of the parameter value, in bytes (this count does not include itself).
+		// Can be zero. As a special case, -1 indicates a NULL parameter value. No value bytes follow in the NULL case.
 		nBytes, err := reader.ReadInt32()
 		if err != nil {
 			return nil, err
 		}
-		bytes := make([]byte, nBytes)
-		nRead, err := reader.Read(bytes)
-		if err != nil {
-			return nil, err
-		}
-		if nRead != int(nBytes) {
-			return nil, newShortMessageErrorWith(int(nBytes), nRead)
+		// The value of the parameter, in the format indicated by the associated format code. n is the above length.
+		var bytes []byte
+		switch nBytes {
+		case 0:
+			bytes = []byte{}
+		case -1:
+			bytes = nil
+		default:
+			if nBytes <= 0 {
+				return nil, newInvalidLengthError(int(nBytes))
+			}
+			bytes = make([]byte, nBytes)
+			nRead, err := reader.Read(bytes)
+			if err != nil {
+				return nil, err
+			}
+			if nRead != int(nBytes) {
+				return nil, newShortMessageError(int(nBytes), nRead)
+			}
 		}
 		var v any
 		if BindParamType(t) == BindParamTypeString {
@@ -99,6 +122,18 @@ func NewBindWith(reader *Reader, parse *Parse) (*Bind, error) {
 			numValues: num,
 			Value:     v,
 		}
+	}
+
+	// The number of result-column format codes that follow (denoted R below).
+	_, err = reader.ReadInt16()
+	if err != nil {
+		return nil, err
+	}
+
+	// The result-column format codes. Each must presently be zero (text) or one (binary).
+	_, err = reader.ReadInt16()
+	if err != nil {
+		return nil, err
 	}
 
 	return &Bind{
