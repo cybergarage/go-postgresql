1c1842a7e1743713111bb6023034cfb86966e4d2
Satoshi Konno <skonno@cybergarage.org>
2023-07-16 00:05:40 +0900

    Update message.RowField::WirteBytes()

--- postgresql/protocol/message/row_field.go
+++ postgresql/protocol/message/row_field.go
@@ -9,18 +9,49 @@ package message
 
 // RowDescription represents a row description field.
 type RowField struct {
-	Name                 string
-	TableOID             int32
-	TableAttributeNumber int16
-	DataTypeOID          int32
-	DataTypeSize         int16
-	TypeModifier         int32
-	FormatCode           int16
+	Name         string
+	TableID      int32
+	Number       int16
+	DataTypeID   int32
+	DataTypeSize int16
+	TypeModifier int32
+	FormatCode   int16
 }
 
 // NewRowField returns a new row description field.
-func NewRowFieldWith(name string) *RowField {
+func NewRowFieldWith(name string, n int) *RowField {
 	return &RowField{
-		Name: name,
+		Name:         name,
+		TableID:      0,
+		Number:       int16(n),
+		DataTypeID:   0,
+		TypeModifier: 0,
+		FormatCode:   0,
 	}
+}
+
+// WirteBytes appends a row field elements.
+func (field *RowField) WirteBytes(w *Writer) error {
+	if err := w.AppendString(field.Name); err != nil {
+		return err
+	}
+	if err := w.AppendInt32(field.TableID); err != nil {
+		return err
+	}
+	if err := w.AppendInt16(field.Number); err != nil {
+		return err
+	}
+	if err := w.AppendInt32(field.DataTypeID); err != nil {
+		return err
+	}
+	if err := w.AppendInt16(field.DataTypeSize); err != nil {
+		return err
+	}
+	if err := w.AppendInt32(field.TypeModifier); err != nil {
+		return err
+	}
+	if err := w.AppendInt16(field.FormatCode); err != nil {
+		return err
+	}
+	return nil
 }
