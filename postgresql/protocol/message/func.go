c03088f0877da354583d8a6f42fa9dc72ca083ac
Satoshi Konno <skonno@cybergarage.org>
2023-07-25 01:05:25 +0900

    Fix message.Int16ToBytes()

--- postgresql/protocol/message/func.go
+++ postgresql/protocol/message/func.go
@@ -36,9 +36,9 @@ func Int16BytesToInt(b []byte) int16 {
 	return v
 }
 
-// Int32ToBytes converts the specified integer to a byte array.
+// Int16ToBytes converts the specified integer to a byte array.
 func Int16ToBytes(v int16) []byte {
-	b := make([]byte, 4)
+	b := make([]byte, 2)
 	b[0] = byte(v >> 8)
 	b[1] = byte(v)
 	return b
