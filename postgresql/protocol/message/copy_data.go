6f962396a27958c11e0be9f7082de60ddea9c0ed
Satoshi Konno <skonno@cybergarage.org>
2023-07-29 02:23:26 +0900

    Add message.CopyData

---
+++ postgresql/protocol/message/copy_data.go
@@ -0,0 +1,37 @@
+// Copyright (C) 2019 Satoshi Konno. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package message
+
+// PostgreSQL: Documentation: 16: 55.2. Message Flow
+// https://www.postgresql.org/docs/16/protocol-flow.html
+// PostgreSQL: Documentation: 16: 55.7. Message Formats
+// https://www.postgresql.org/docs/16/protocol-message-formats.html
+
+// CopyData represents a copy data message.
+type CopyData struct {
+	MessageLength int32
+}
+
+// NewCopyDataWithReader returns a new copy data message with the specified reader.
+func NewCopyDataWithReader(reader *Reader) (*CopyData, error) {
+	msgLen, err := reader.ReadInt32()
+	if err != nil {
+		return nil, err
+	}
+
+	return &CopyData{
+		MessageLength: msgLen,
+	}, nil
+}
