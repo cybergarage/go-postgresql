d07674b598e816541b7677b9e269f5c9bfa62a60
Satoshi Konno <skonno@cybergarage.org>
2023-08-01 19:51:49 +0900

    Add NewCopyStreamWith()

---
+++ postgresql/copy.go
@@ -0,0 +1,45 @@
+// Copyright (C) 2019 Satoshi Konno. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package postgresql
+
+import (
+	"github.com/cybergarage/go-postgresql/postgresql/protocol/message"
+)
+
+type CopyStream struct {
+	*message.RequestMessage
+}
+
+func NewCopyStreamWith(reqMsg *message.RequestMessage) *CopyStream {
+	return &CopyStream{
+		RequestMessage: reqMsg,
+	}
+}
+
+func (stream *CopyStream) Next() (*message.DataRow, error) {
+	reqType, err := stream.ReadType()
+	if err != nil {
+		return nil, err
+	}
+
+	switch reqType { // nolint:exhaustive
+	case message.CopyDataMessage:
+	case message.CopyDoneMessage:
+	default:
+		return nil, message.NewErrInvalidMessage(reqType)
+	}
+
+	return nil, nil
+}
