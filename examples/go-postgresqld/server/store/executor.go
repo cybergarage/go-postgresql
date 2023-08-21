f4aa2d301d4071fceafd79982744cbb6583fbbb2
Satoshi Konno <skonno@cybergarage.org>
2023-08-22 08:30:12 +0900

    Update MemTable::DropTable()

--- examples/go-postgresqld/server/store/executor.go
+++ examples/go-postgresqld/server/store/executor.go
@@ -104,6 +104,9 @@ func (store *MemStore) DropTable(conn *postgresql.Conn, q *query.DropTable) (mes
 	for _, dropTbl := range q.Tables() {
 		db, tbl, err := store.GetDatabaseTable(conn, conn.DatabaseName(), dropTbl.TableName())
 		if err != nil {
+			if q.IfExists() {
+				continue
+			}
 			return nil, err
 		}
 		err = db.DropTable(tbl)
