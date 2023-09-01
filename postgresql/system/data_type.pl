10df5cd418331e6587f23e5a22b4defbaf2d691d
Satoshi Konno <skonno@cybergarage.org>
2023-09-02 01:17:42 +0900

    Update system.DataType::Size()

--- postgresql/system/data_type.pl
+++ postgresql/system/data_type.pl
@@ -23,6 +23,7 @@ package system
 type DataType struct {
 	name string
 	oid  OID
+	size int
 }
 
 // DataType represents a PostgreSQL data type.
@@ -37,10 +38,11 @@ func GetDataType(oid OID) (*DataType, error) {
 	return dt, nil
 }
 
-func newDataType(name string, oid OID) *DataType {
+func newDataType(name string, oid OID, size int) *DataType {
 	dataType := &DataType{
 		name: name,
 		oid:  oid,
+		size: size,
 	}
 	return dataType
 }
@@ -55,16 +57,23 @@ func (dataType *DataType) OID() OID {
 	return dataType.oid
 }
 
+// Size returns the data type size.
+func (dataType *DataType) Size() int {
+	return dataType.size
+}
+
 func init() { //nolint: gochecknoinits, maintidx
 HEADER
 
 my $pg_type_file = "res/pg_type.csv";
 open(IN, $pg_type_file) or die "Failed to open $pg_type_file: $!";
-$line_no = 0;
-$name_idx = -1;
-$oid_idx = -1;
+my $line_no = 0;
+my $name_idx = -1;
+my $oid_idx = -1;
+my $typ_len_idx = -1;
 while(<IN>){
   $line_no++;
+  chomp($_);
   my @row = split(/,/, $_, -1);
   if ($line_no <= 1) {
     for (my $i = 0; $i < scalar(@row); $i++) {
@@ -74,16 +83,30 @@ while(<IN>){
       if ($row[$i] eq "oid") {
         $oid_idx = $i;
       }
+      if ($row[$i] eq "typlen") {
+        $typ_len_idx = $i;
+      }      
     }
-    if ($name_idx < 0 || $oid_idx < 0) {
-      die "Failed to find typname or oid column";
+    if ($name_idx < 0) {
+      die "Failed to find typname column";
+    }
+    if ($oid_idx < 0) {
+      die "Failed to find oid column";
+    }
+    if ($typ_len_idx < 0) {
+      die "Failed to find typlen column";
     }
     next;
   }
-  chomp($_);
   my $name = $row[$name_idx];
   my $oid = $row[$oid_idx];
+  my $size = $row[$typ_len_idx];
+
   if ($name =~ /^_/){
+    next;
+  }
+  # check nil rows
+  if (length($name)<=0 || length($oid)<=0 || length($size)<=0) {
     next;
   }
   # UpperCase
@@ -91,10 +114,6 @@ while(<IN>){
   # CamelCase
   $name =~ s/_([a-z])/\u$1/g;
   $name =~ s/^([a-z])/\u$1/;
-  # check nil rows
-  if (length($name)<=0 || length($oid)<=0) {
-    next;
-  }
   # stylecheck
   my @patterns = ('Sql', 'Xml', 'Json', 'Uuid', 'Db', 'Ts', 'Acl');
   my @replacements = ('SQL', 'XML', 'JSON', 'UUID', 'DB', 'TS', 'ACL');
@@ -103,8 +122,7 @@ while(<IN>){
       $name =~ s/$patterns[$i]/$replacements[$i]/g;
     }
   }
-
-  print "\tdataTypes[" . $oid . "] = newDataType(\"" . $name . "\", " . $oid . ")\n";  }
+  print "\tdataTypes[" . $oid . "] = newDataType(\"" . $name . "\", " . $oid . ", " . $size . ")\n";  }
   close(IN);
 
 print<<FOTTER;
