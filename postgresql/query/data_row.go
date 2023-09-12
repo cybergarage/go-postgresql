3d833ac59e6fbcb61cc8032935f5c9f1e6c1d0d7
Satoshi Konno <skonno@cybergarage.org>
2023-09-12 20:13:58 +0900

    Update NewDataRowForSelectors()

---
+++ postgresql/query/data_row.go
@@ -0,0 +1,164 @@
+// Copyright (C) 2019 Satoshi Konno. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package query
+
+import (
+	"fmt"
+
+	"github.com/cybergarage/go-postgresql/postgresql/protocol/message"
+	"github.com/cybergarage/go-sqlparser/sql/query"
+)
+
+// Row represents a row of a result set.
+type Row = map[string]any
+
+// NewDataRowForSelectors returns a new DataRow from the specified row.
+func NewDataRowForSelectors(schema *query.Schema, rowDesc *message.RowDescription, selectors query.SelectorList, row Row) (*message.DataRow, error) {
+	dataRow := message.NewDataRow()
+	for n, selector := range selectors {
+		field := rowDesc.Field(n)
+		switch selector := selector.(type) {
+		case *query.Column:
+			name := selector.Name()
+			v, ok := row[name]
+			if !ok {
+				dataRow.AppendData(field, nil)
+				continue
+			}
+			dataRow.AppendData(field, v)
+		case *query.Function:
+			executor, err := selector.Executor()
+			if err != nil {
+				return nil, err
+			}
+			args := []any{}
+			for _, arg := range selector.Arguments() {
+				v, ok := row[arg.Name()]
+				if !ok {
+					v = nil
+				}
+				args = append(args, v)
+			}
+			v, err := executor.Execute(args...)
+			if err != nil {
+				return nil, err
+			}
+			dataRow.AppendData(field, v)
+		}
+	}
+	return dataRow, nil
+}
+
+// NewDataRowsForAggregateFunction returns a new DataRow list from the specified rows.
+func NewDataRowsForAggregateFunction(schema *query.Schema, rowDesc *message.RowDescription, selectors query.SelectorList, rows []Row, groupBy string) ([]*message.DataRow, error) {
+	// Setups aggregate functions
+	aggrFns := []*query.Function{}
+	aggrExecutors := []*query.AggregateFunction{}
+	for _, selector := range selectors {
+		fn, ok := selector.(*query.Function)
+		if !ok {
+			continue
+		}
+		executor, err := fn.Executor()
+		if err != nil {
+			return nil, err
+		}
+		aggrExecutor, ok := executor.(*query.AggregateFunction)
+		if !ok {
+			return nil, fmt.Errorf("invalid aggregate function (%s)", fn.Name())
+		}
+		aggrFns = append(aggrFns, fn)
+		aggrExecutors = append(aggrExecutors, aggrExecutor)
+	}
+	// Executes aggregate functions
+	for _, row := range rows {
+		for n, aggrFn := range aggrFns {
+			var groupKey any
+			groupKey = ""
+			if 0 < len(groupBy) {
+				v, ok := row[groupBy]
+				if !ok {
+					return nil, NewErrGroupByColumnValueNotExist(groupBy)
+				}
+				groupKey = v
+			}
+			args := []any{
+				groupKey,
+			}
+			for _, arg := range aggrFn.Arguments() {
+				if arg.IsAsterisk() {
+					args = append(args, arg.Name())
+					continue
+				}
+				v, ok := row[arg.Name()]
+				if !ok {
+					return nil, NewErrColumnValueNotExist(arg.Name())
+				}
+				args = append(args, v)
+			}
+			_, err := aggrExecutors[n].Execute(args...)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+	// Add aggregate results
+	aggrResultSets := map[string]query.AggregateResultSet{}
+	groupKeys := []any{}
+	for _, aggaggrExecutor := range aggrExecutors {
+		aggResultSet := aggaggrExecutor.ResultSet()
+		aggrResultSets[aggaggrExecutor.Name()] = aggResultSet
+		for aggrResultKey := range aggResultSet {
+			hasGroupKey := false
+			for _, groupKey := range groupKeys {
+				if groupKey == aggrResultKey {
+					hasGroupKey = true
+				}
+			}
+			if hasGroupKey {
+				continue
+			}
+			groupKeys = append(groupKeys, aggrResultKey)
+		}
+	}
+	dataRows := []*message.DataRow{}
+	for _, groupKey := range groupKeys {
+		dataRow := message.NewDataRow()
+		for n, selector := range selectors {
+			field := rowDesc.Field(n)
+			name := selector.Name()
+			switch selector.(type) {
+			case *query.Column:
+				if name != groupBy {
+					return nil, fmt.Errorf("invalid column (%s)", name)
+				}
+				dataRow.AppendData(field, groupKey)
+			case *query.Function:
+				aggResultSet, ok := aggrResultSets[name]
+				if !ok {
+					return nil, fmt.Errorf("invalid aggregate function (%s)", name)
+				}
+				aggResult, ok := aggResultSet[groupKey]
+				if ok {
+					dataRow.AppendData(field, aggResult)
+				} else {
+					dataRow.AppendData(field, nil)
+				}
+			}
+		}
+		dataRows = append(dataRows, dataRow)
+	}
+	return dataRows, nil
+}
