eed268a6b103959eacb197a0e3ff2e732f5607f8
Satoshi Konno <skonno@cybergarage.org>
2023-09-12 20:11:21 +0900

    Update query.NewRowFieldFrom()

---
+++ postgresql/query/row_field.go
@@ -0,0 +1,68 @@
+// Copyright (C) 2019 The go-postgresql Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package query
+
+import (
+	"fmt"
+
+	"github.com/cybergarage/go-postgresql/postgresql/protocol/message"
+	"github.com/cybergarage/go-postgresql/postgresql/system"
+	"github.com/cybergarage/go-sqlparser/sql/query"
+)
+
+// NewRowFieldFrom returns a new RowField from the specified selector.
+func NewRowFieldFrom(schema *query.Schema, selector query.Selector, idx int) (*message.RowField, error) {
+	var columnName string
+	var dt *system.DataType
+	var err error
+	switch selector := selector.(type) {
+	case *query.Column:
+		columnName = selector.Name()
+		schemaColumn, err := schema.ColumnByName(columnName)
+		if err != nil {
+			return nil, err
+		}
+		dt, err = NewDataTypeFrom(schemaColumn.DataType())
+		if err != nil {
+			return nil, err
+		}
+	case *query.Function:
+		if !selector.IsSelectAll() {
+			args := selector.Arguments()
+			if len(args) != 1 {
+				return nil, fmt.Errorf("multiple arguments (%v)", args)
+			}
+			columnName = args[0].Name()
+			schemaColumn, err := schema.ColumnByName(columnName)
+			if err != nil {
+				return nil, err
+			}
+			dt, err = NewDataTypeFrom(schemaColumn.DataType())
+			if err != nil {
+				return nil, err
+			}
+		}
+		dt, err = system.GetFunctionDataType(selector, dt)
+		if err != nil {
+			return nil, err
+		}
+		columnName = selector.SelectorString()
+	}
+	return message.NewRowFieldWith(columnName,
+		message.WithNumber(int16(idx+1)),
+		message.WithDataTypeID(dt.OID()),
+		message.WithDataTypeSize(int16(dt.Size())),
+		message.WithFormatCode(dt.FormatCode()),
+	), nil
+}
