6e9853fb274eec1cd557880fcda5cc4a199792b7
Satoshi Konno <skonno@cybergarage.org>
2024-11-08 19:13:31 +0900

    Update net.ConnManager

--- postgresql/net/conn_manager.go
+++ postgresql/net/conn_manager.go
@@ -15,87 +15,17 @@
 package net
 
 import (
-	"errors"
-	"sync"
-
-	"github.com/google/uuid"
+	"github.com/cybergarage/go-sqlparser/sql/net"
 )
 
 // ConnManager represents a connection map.
 type ConnManager struct {
-	m     map[uuid.UUID]Conn
-	mutex *sync.RWMutex
+	*net.ConnManager
 }
 
 // NewConnManager returns a connection map.
 func NewConnManager() *ConnManager {
 	return &ConnManager{
-		m:     map[uuid.UUID]Conn{},
-		mutex: &sync.RWMutex{},
+		net.NewConnManager(),
 	}
-}
-
-// AddConn adds the specified connection.
-func (mgr *ConnManager) AddConn(c Conn) {
-	mgr.mutex.Lock()
-	defer mgr.mutex.Unlock()
-	uuid := c.UUID()
-	mgr.m[uuid] = c
-}
-
-// Conns returns the included connections.
-func (mgr *ConnManager) Conns() []Conn {
-	mgr.mutex.RLock()
-	defer mgr.mutex.RUnlock()
-	conns := make([]Conn, 0, len(mgr.m))
-	for _, conn := range mgr.m {
-		conns = append(conns, conn)
-	}
-	return conns
-}
-
-// ConnByUUID returns the connection with the specified UUID.
-func (mgr *ConnManager) ConnByUUID(uuid uuid.UUID) (Conn, bool) {
-	mgr.mutex.RLock()
-	defer mgr.mutex.RUnlock()
-	c, ok := mgr.m[uuid]
-	return c, ok
-}
-
-// RemoveConn deletes the specified connection from the map.
-func (mgr *ConnManager) RemoveConn(conn Conn) error {
-	mgr.mutex.Lock()
-	defer mgr.mutex.Unlock()
-	delete(mgr.m, conn.UUID())
-	return nil
-}
-
-// Start starts the connection manager.
-func (mgr *ConnManager) Start() error {
-	return nil
-}
-
-// Close closes the connection manager.
-func (mgr *ConnManager) Close() error {
-	var errs error
-	conns := mgr.Conns()
-	for _, conn := range conns {
-		err := conn.Close()
-		if err == nil {
-			if err := mgr.RemoveConn(conn); err != nil {
-				errs = errors.Join(errs, err)
-			}
-		} else {
-			errs = errors.Join(errs, err)
-		}
-	}
-	return errs
-}
-
-// Stop closes all connections.
-func (mgr *ConnManager) Stop() error {
-	if err := mgr.Close(); err != nil {
-		return err
-	}
-	return nil
 }
