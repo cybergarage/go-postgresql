8813621e9c66ddc7049664c138321c7a84103f4f
Satoshi Konno <skonno@cybergarage.org>
2025-02-22 00:47:45 +0900

    Update TestSQLTest()

--- postgresqltest/sqltest/sql_test.go
+++ postgresqltest/sqltest/sql_test.go
@@ -38,6 +38,7 @@ func TestSQLTest(t *testing.T) {
 
 	// NOTE: Add your test files in 'untests' directory into the filename array
 	testNames := []string{
+		"SmplCrudInt",
 		// "SmplCrudText",
 		// "SmplCrudDatetime",
 	}
