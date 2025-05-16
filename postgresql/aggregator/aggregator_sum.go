ce0cebc5f1c4153e8037dd211406272b4e226221
Satoshi Konno <skonno@cybergarage.org>
2025-05-16 22:20:42 +0900

    Update aggregator.Sum::Reset()

--- postgresql/aggregator/aggregator_sum.go
+++ postgresql/aggregator/aggregator_sum.go
@@ -19,13 +19,18 @@ import (
 )
 
 type Sum struct {
+	// colums is the list of columns to sum.
 	colums []string
+	// sums is the sums of the values.
+	sums []float64
+	// counts is the number of values.
+	counts []int
 	// groupBy is the name of the column to group by.
 	groupBy string
-	// sum is the sum of the values.
-	sum map[any]float64
-	// count is the number of values.
-	count map[any]int
+	// groupSums is the sum of the values for each group.
+	groupSums map[any][]float64
+	// groupCounts is the count of the values for each group.
+	groupCounts map[any][]int
 }
 
 // SumOption is a function that configures the Sum aggregator.
@@ -34,23 +39,22 @@ type SumOption func(*Sum) error
 // NewSum creates a new Sum aggregator with the given options.
 func NewSum(options ...SumOption) (*Sum, error) {
 	s := &Sum{
-		colums:  make([]string, 0),
-		groupBy: "",
-		sum:     make(map[any]float64),
-		count:   make(map[any]int),
+		colums:      make([]string, 0),
+		groupBy:     "",
+		sums:        make([]float64, 0),
+		counts:      make([]int, 0),
+		groupSums:   make(map[any][]float64),
+		groupCounts: make(map[any][]int),
 	}
+
 	for _, opt := range options {
 		if err := opt(s); err != nil {
 			return nil, err
 		}
 	}
 
-	// Validate the arguments
-	if len(s.colums) == 0 {
-		return nil, fmt.Errorf("no argument %w", ErrNotSupported)
-	}
-	if len(s.colums) > 1 {
-		return nil, fmt.Errorf("multiple argument %w", ErrNotSupported)
+	if err := s.Reset(); err != nil {
+		return nil, err
 	}
 
 	return s, nil
@@ -77,29 +81,83 @@ func (s *Sum) Name() string {
 	return "sum"
 }
 
+// GroupBy returns the group by column name and a boolean indicating if it is set.
+func (s *Sum) GroupBy() (string, bool) {
+	if len(s.groupBy) == 0 {
+		return "", false
+	}
+	return s.groupBy, true
+}
+
 // Reset resets the aggregator to its initial state.
 func (s *Sum) Reset() error {
 	s.colums = []string{}
-	if 0 < len(s.groupBy) {
-		s.colums = append(s.colums, s.groupBy)
+	if groupBy, ok := s.GroupBy(); ok {
+		s.colums = append(s.colums, groupBy)
 	}
 	for _, arg := range s.colums {
 		s.colums = append(s.colums, fmt.Sprintf("%s(%s)", s.Name(), arg))
 	}
 
-	s.sum = make(map[any]float64)
-	s.count = make(map[any]int)
+	// Validate the arguments
+
+	if len(s.colums) == 0 {
+		return fmt.Errorf("no argument %w", ErrNotSupported)
+	}
+	if 1 < len(s.colums) {
+		return fmt.Errorf("multiple argument %w", ErrNotSupported)
+	}
+
+	// Reset aggregator variables
+
+	s.sums = make([]float64, len(s.colums))
+	s.counts = make([]int, len(s.colums))
+
+	s.groupSums = make(map[any][]float64)
+	s.groupCounts = make(map[any][]int)
+
 	return nil
 }
 
 // Aggregate aggregates a row of data.
 func (s *Sum) Aggregate(row Row) error {
+	if len(s.colums) != len(row) {
+		return fmt.Errorf("%w column count (%d != %d)", ErrInvalid, len(s.colums), len(row))
+	}
+	// for _, rowValue := range row {
+	// 	if _, ok := s.GroupBy(); ok {
+	// 		group := row[0]
+	// 		if _, ok := s.groupSum[group]; !ok {
+	// 			s.groupSum[group] = 0
+	// 			s.groupCount[group] = 0
+	// 		}
+	// 		s.groupSum[group] += row[1].Float64()
+	// 		s.groupCount[group]++
+	// 	} else {
+	// 		s.sum += row[0].Float64()
+	// 		s.count++
+	// 	}
+	// }
 	return nil
 }
 
 // Finalize finalizes the aggregation and returns the result.
 func (s *Sum) Finalize() (ResultSet, error) {
+	rows := make([]Row, 0)
+	if _, ok := s.GroupBy(); ok {
+		for key, value := range s.groupSums {
+			row := make([]any, 0)
+			row = append(row, key)
+			row = append(row, value)
+			rows = append(rows, row)
+		}
+	} else {
+		row := make([]any, 0)
+		row = append(row, s.sums)
+		rows = append(rows, row)
+	}
 	return NewResultSet(
+		WithRows(rows),
 		WithColumns(s.colums),
 	), nil
 }
