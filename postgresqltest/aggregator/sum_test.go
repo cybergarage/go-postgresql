9467916a65ced9eb64f82910af9f6cf19c22f2e6
Satoshi Konno <skonno@cybergarage.org>
2025-05-17 00:25:31 +0900

    Update TestSum()

--- postgresqltest/aggregator/sum_test.go
+++ postgresqltest/aggregator/sum_test.go
@@ -17,9 +17,91 @@ package aggregator
 import (
 	"testing"
 
+	"github.com/cybergarage/go-postgresql/postgresql/aggregator"
+
 	"github.com/cybergarage/go-logger/log"
 )
 
 func TestSum(t *testing.T) {
 	log.SetStdoutDebugEnbled(true)
+
+	tests := []struct {
+		args             []string
+		rows             []aggregator.Row
+		expectedSums     [][]float64
+		expectedRowCount int
+	}{
+		{
+			args: []string{"foo"},
+			rows: []aggregator.Row{
+				{1},
+				{2},
+				{3},
+			},
+			expectedSums:     [][]float64{{6}},
+			expectedRowCount: 1,
+		},
+		{
+			args: []string{"foo"},
+			rows: []aggregator.Row{
+				{1},
+				{2},
+				{3},
+				{4},
+			},
+			expectedSums:     [][]float64{{10}},
+			expectedRowCount: 1,
+		},
+	}
+
+	for _, test := range tests {
+		aggr, err := aggregator.NewSum(
+			aggregator.WithSubArguments(test.args...),
+		)
+		if err != nil {
+			t.Errorf("Error creating Sum: %v", err)
+			continue
+		}
+
+		for _, row := range test.rows {
+			if err := aggr.Aggregate(row); err != nil {
+				t.Errorf("Error adding row: %v", err)
+				continue
+			}
+		}
+
+		rs, err := aggr.Finalize()
+		if err != nil {
+			t.Errorf("Error finalizing Sum: %v", err)
+			continue
+		}
+
+		rsRows := []aggregator.Row{}
+		for rs.Next() {
+			row, err := rs.Row()
+			if err != nil {
+				t.Errorf("Error getting row: %v", err)
+				continue
+			}
+			rsRows = append(rsRows, row)
+		}
+
+		if len(rsRows) != test.expectedRowCount {
+			t.Errorf("Expected %d rows, got %d", test.expectedRowCount, len(rsRows))
+			continue
+		}
+
+		for i, expectedSum := range test.expectedSums {
+			if len(rsRows[i]) != len(expectedSum) {
+				t.Errorf("Expected %d columns, got %d", len(expectedSum), len(rsRows[i]))
+				continue
+			}
+			for j, expected := range expectedSum {
+				if rsRows[i][j] != expected {
+					t.Errorf("Expected %f, got %f", expected, rsRows[i][j])
+					continue
+				}
+			}
+		}
+	}
 }
