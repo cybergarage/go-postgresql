daca5e162b66e555706458063548868595151355
Satoshi Konno <skonno@cybergarage.org>
2025-05-18 01:34:50 +0900

    Update TestAggregators()

--- postgresqltest/aggregator/sum_test.go
+++ postgresqltest/aggregator/sum_test.go
@@ -24,7 +24,7 @@ import (
 	"github.com/cybergarage/go-logger/log"
 )
 
-func TestSum(t *testing.T) {
+func TestAggregators(t *testing.T) {
 	log.SetStdoutDebugEnbled(true)
 
 	tests := []struct {
@@ -32,6 +32,7 @@ func TestSum(t *testing.T) {
 		args             []string
 		rows             []aggregator.Row
 		expectedSumRows  [][]float64
+		expectedAvgRows  [][]float64
 		expectedRowCount int
 	}{
 		{
@@ -43,6 +44,7 @@ func TestSum(t *testing.T) {
 				{3},
 			},
 			expectedSumRows:  [][]float64{{6}},
+			expectedAvgRows:  [][]float64{{2}},
 			expectedRowCount: 1,
 		},
 		{
@@ -55,6 +57,7 @@ func TestSum(t *testing.T) {
 				{4},
 			},
 			expectedSumRows:  [][]float64{{10}},
+			expectedAvgRows:  [][]float64{{2.5}},
 			expectedRowCount: 1,
 		},
 		{
@@ -67,6 +70,7 @@ func TestSum(t *testing.T) {
 				{4, 4},
 			},
 			expectedSumRows:  [][]float64{{1, 1}, {2, 2}, {3, 3}, {4, 4}},
+			expectedAvgRows:  [][]float64{{1, 1}, {2, 2}, {3, 3}, {4, 4}},
 			expectedRowCount: 4,
 		},
 		{
@@ -83,75 +87,111 @@ func TestSum(t *testing.T) {
 				{4, 4},
 			},
 			expectedSumRows:  [][]float64{{1, 2}, {2, 4}, {3, 6}, {4, 8}},
+			expectedAvgRows:  [][]float64{{1, 1}, {2, 2}, {3, 3}, {4, 4}},
 			expectedRowCount: 4,
 		},
 	}
 
 	for _, test := range tests {
-		aggr, err := aggregator.NewSum(
-			aggregator.WithSumGroupBy(test.orderBy),
-			aggregator.WithSumArguments(test.args...),
-		)
-		if err != nil {
-			t.Errorf("Error creating Sum: %v", err)
-			continue
+
+		aggrsFunc := []func() (aggregator.Aggregator, error){
+			func() (aggregator.Aggregator, error) {
+				return aggregator.NewSum(
+					aggregator.WithSumGroupBy(test.orderBy),
+					aggregator.WithSumArguments(test.args...),
+				)
+			},
+			func() (aggregator.Aggregator, error) {
+				return aggregator.NewAvg(
+					aggregator.WithAvgGroupBy(test.orderBy),
+					aggregator.WithAvgArguments(test.args...),
+				)
+			},
 		}
 
-		for _, row := range test.rows {
-			if err := aggr.Aggregate(row); err != nil {
-				t.Errorf("Error adding row: %v", err)
-				continue
-			}
-		}
+		for _, aggrs := range aggrsFunc {
 
-		rs, err := aggr.Finalize()
-		if err != nil {
-			t.Errorf("Error finalizing Sum: %v", err)
-			continue
-		}
+			// Aggregate
 
-		rsRows := []aggregator.Row{}
-		for rs.Next() {
-			row, err := rs.Row()
+			aggr, err := aggrs()
 			if err != nil {
-				t.Errorf("Error getting row: %v", err)
+				t.Error(err)
 				continue
 			}
-			rsRows = append(rsRows, row)
-		}
 
-		sort.Slice(rsRows, func(i, j int) bool {
-			var ii, ij int
-			if err := safecast.ToInt(rsRows[i][0], &ii); err != nil {
-				return false
-			}
-			if err := safecast.ToInt(rsRows[j][0], &ij); err != nil {
-				return false
-			}
-			return ii < ij
-		})
+			t.Run(aggr.Name(), func(t *testing.T) {
 
-		if len(rsRows) != test.expectedRowCount {
-			t.Errorf("Expected %d rows, got %d", test.expectedRowCount, len(rsRows))
-			continue
-		}
-
-		for n, expectedSumRow := range test.expectedSumRows {
-			if len(rsRows[n]) != len(expectedSumRow) {
-				t.Errorf("Expected %d columns, got %d", len(expectedSumRow), len(rsRows[n]))
-				continue
-			}
-			for i, expectedSum := range expectedSumRow {
-				var rowValue float64
-				if err := safecast.ToFloat64(rsRows[n][i], &rowValue); err != nil {
-					t.Errorf("Error converting row value to int: %v", err)
-					continue
+				for _, row := range test.rows {
+					if err := aggr.Aggregate(row); err != nil {
+						t.Errorf("Error adding row: %v", err)
+						continue
+					}
 				}
-				if rowValue != expectedSum {
-					t.Errorf("Expected %f, got %f", expectedSum, rowValue)
-					continue
+
+				rs, err := aggr.Finalize()
+				if err != nil {
+					t.Errorf("Error finalizing Sum: %v", err)
+					return
 				}
-			}
+
+				rsRows := []aggregator.Row{}
+				for rs.Next() {
+					row, err := rs.Row()
+					if err != nil {
+						t.Errorf("Error getting row: %v", err)
+						continue
+					}
+					rsRows = append(rsRows, row)
+				}
+
+				sort.Slice(rsRows, func(i, j int) bool {
+					var ii, ij int
+					if err := safecast.ToInt(rsRows[i][0], &ii); err != nil {
+						return false
+					}
+					if err := safecast.ToInt(rsRows[j][0], &ij); err != nil {
+						return false
+					}
+					return ii < ij
+				})
+
+				if len(rsRows) != test.expectedRowCount {
+					t.Errorf("Expected %d rows, got %d", test.expectedRowCount, len(rsRows))
+					return
+				}
+
+				// Compare the result set with the expected rows
+
+				var expectedRows [][]float64
+
+				switch aggr.(type) {
+				case *aggregator.Sum:
+					expectedRows = test.expectedSumRows
+				case *aggregator.Avg:
+					expectedRows = test.expectedAvgRows
+				default:
+					t.Errorf("Unexpected aggregator type: %T", aggr)
+					return
+				}
+
+				for n, expectedRow := range expectedRows {
+					if len(rsRows[n]) != len(expectedRow) {
+						t.Errorf("Expected %d columns, got %d", len(expectedRow), len(rsRows[n]))
+						continue
+					}
+					for i, expectedSum := range expectedRow {
+						var rowValue float64
+						if err := safecast.ToFloat64(rsRows[n][i], &rowValue); err != nil {
+							t.Errorf("Error converting row value to int: %v", err)
+							continue
+						}
+						if rowValue != expectedSum {
+							t.Errorf("Expected %f, got %f", expectedSum, rowValue)
+							continue
+						}
+					}
+				}
+			})
 		}
 	}
 }
