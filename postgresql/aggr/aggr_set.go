ff79d2e26f24ced420a5dc463a1e17370d31dc6c
Satoshi Konno <skonno@cybergarage.org>
2025-05-21 12:29:15 +0900

    Add aggr.AggregatorSet()

---
+++ postgresql/aggr/aggr_set.go
@@ -0,0 +1,30 @@
+// Copyright (C) 2019 The go-postgresql Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package aggr
+
+type AggregatorSet []Aggregator
+
+// NewAggregatorSetForNames creates a new AggregatorSet for the given names.
+func NewAggregatorSetForNames(names []string, opts ...aggrOption) (AggregatorSet, error) {
+	aggrSet := make(AggregatorSet, len(names))
+	for i, name := range names {
+		aggr, err := NewAggregatorForName(name, opts...)
+		if err != nil {
+			return nil, err
+		}
+		aggrSet[i] = aggr
+	}
+	return aggrSet, nil
+}
