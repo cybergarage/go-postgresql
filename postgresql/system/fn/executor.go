d70501730e0e4f82acb3946f828dd96a2b627b8e
Satoshi Konno <skonno@cybergarage.org>
2025-11-07 20:19:31 +0900

    feat: Add system.fn package

---
+++ postgresql/system/fn/executor.go
@@ -0,0 +1,30 @@
+// Copyright (C) 2022 The go-postgresql Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fn
+
+import (
+	"github.com/cybergarage/go-sqlparser/sql/fn"
+)
+
+// Executor represents a function executor.
+type Executor = fn.Executor
+
+// ExecutorOption represents a function executor option.
+type ExecutorOption = fn.ExecutorOption
+
+// NewExecutorForName returns a function executor with the specified name.
+func NewExecutorForName(name string, opts ...ExecutorOption) (Executor, error) {
+	return fn.NewExecutorForName(name, opts...)
+}
